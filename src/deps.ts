8763f6d8877145777ea0505ebad70934d28de9ab
Patricio Zavolinsky <pzavolinsky@yahoo.com.ar>
2016-07-24 18:22:05 +0100

    Full project dependency analysis

--- src/deps.ts
+++ src/deps.ts
@@ -11,60 +11,111 @@ interface Dups {
   [index:string]:boolean
 }
 
+interface Dependency {
+  name:         string
+  depth:        number
+  count:        number
+  dependencies: Dependency[]
+}
+
+interface DepAnalysis {
+  [index:string]:Dependency
+}
+
 const getFileMap = (files:File[]):FileMap => {
   const map:FileMap = {};
   files.map(f => map[f.path] = f);
   return map;
 };
 
-function dumpFile(
+function analyzeFile(
   fileMap:FileMap,
   file:File,
-  dups:Dups,
-  padd:string = ''
-):void {
-  if (dups[file.path]) {
-    console.log(`${padd}${file.path} [dup]`);
-    return;
-  }
+  analysis:DepAnalysis
+):Dependency {
+  const existing = analysis[file.path];
 
-  console.log(`${padd}${file.path}`);
-  dups[file.path] = true;
-  Object.keys(file.imports).map(i =>
-    dumpFile(
+  if (existing) return existing;
+
+  const dep:Dependency = {
+    name:  file.path,
+    count: 1,
+    depth: 1,
+    dependencies: []
+  };
+
+  analysis[file.path] = dep;
+
+  const deps = Object.keys(file.imports).map(d =>
+    analyzeFile(
       fileMap,
-      fileMap[i],
-      dups,
-      `${padd} `
+      fileMap[d],
+      analysis
     )
   );
+
+  const depth = deps.map(d => d.depth).reduce((a, b) => Math.max(a, b), 0);
+  const count = deps.map(d => d.count).reduce((a, b) => a + b, 0);
+  dep.depth = 1 + depth;
+  dep.count = 1 + count;
+  dep.dependencies = deps;
+
+  return dep;
 };
-const dumpDeps = (tsconfigPath:string, moduleName:string) => {
+
+const analyzeDeps = (tsconfigPath:string) => {
   const files = parseFiles(
     dirname(tsconfigPath),
     JSON.parse(readFileSync(tsconfigPath, { encoding: 'utf8' })).files
   );
   const fileMap = getFileMap(files);
-  const file = fileMap[moduleName];
-  if (!file) {
-    console.error(`cannot find '${moduleName}', valid modules are:\n${
-      files.map(f => `  - ${f.path}`).join('\n')
-    }`);
-    process.exit(-1);
-  }
 
-  const dups:Dups = {};
+  const analysis:DepAnalysis = {};
 
-  dumpFile(fileMap, file, dups);
-  console.log(`${Object.keys(dups).length} nodes in the dependency tree`);
+  files.forEach(f => analyzeFile(fileMap, f, analysis));
+
+  return analysis;
 };
 
-const args = process.argv.slice(2);
-const [ tsconfig, file ] = args;
+const [ tsconfig, filter ] = process.argv.slice(2);
 
 if (!tsconfig || !existsSync(tsconfig) || !statSync(tsconfig).isFile()) {
-  console.error(`usage: deps path/to/tsconfig.json path/to/module`);
+  console.error(`usage: deps path/to/tsconfig.json [filter]`);
   process.exit(-1);
 }
 
-dumpDeps(tsconfig, file);
+const getValues = (o:DepAnalysis) =>
+  Object.keys(o).reduce<Dependency[]>(
+    (v, k) => v.concat([ o[k] ]),
+    []
+  );
+
+const analysis = analyzeDeps(tsconfig);
+const deps = getValues(analysis);
+deps.sort((a, b) => a.depth - b.depth);
+
+console.log(`${deps.length} modules found`);
+
+function dumpDep(dep:Dependency, dups:Dups = {}, padd:string = ''):boolean {
+  const dup = dups[dep.name] ? ' [dup]' : '';
+  console.log(`${padd}[${dep.depth - 1}|${dep.count - 1}] ${dep.name}${dup}`);
+  return !dup;
+};
+
+function dumpDepRec(dep:Dependency, dups:Dups = {}, padd:string = ''):void {
+  if (!dumpDep(dep, dups, padd)) return;
+  dups[dep.name] = true;
+  dep.dependencies.forEach(d => dumpDepRec(d, dups, `${padd}  `));
+};
+
+deps.forEach(d => dumpDep(d));
+
+console.log(`${Object.keys(analysis).length} nodes in the dependency tree`);
+
+if (filter) {
+  const re = new RegExp(filter, 'i');
+  deps.filter(d => d.name.match(re)).forEach(d => {
+    console.log('-----------------');
+    dumpDepRec(d);
+  });
+}
