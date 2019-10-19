dd0eb50a7be1247e9553797fb9d000970e214724
sean <str_ie@yahoo.co.uk>
2019-10-19 12:20:33 +0200

    Remove built files from git

--- lib/ArgsParser.d.ts
+++
@@ -1,7 +0,0 @@
-import { ExtraCommandLineOptions } from "./types";
-export declare type TsFilesAndOptions = {
-    tsFiles?: string[];
-    options: ExtraCommandLineOptions;
-};
-declare const _default: (files?: string[] | undefined) => TsFilesAndOptions;
-export default _default;
