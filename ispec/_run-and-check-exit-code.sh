efad500797272fc9272bba8b816c6481ffb68ba2
sean <str_ie@yahoo.co.uk>
2019-10-27 20:41:21 +0100

    Add new option --showLineNumber to itests

--- ispec/_run-and-check-exit-code.sh
+++ ispec/_run-and-check-exit-code.sh
@@ -1,11 +1,16 @@
 # continue on error
 set +e;
-node ../../bin/ts-unused-exports tsconfig.json --exitWithCount --ignorePaths=to-ignore
-ERROR_COUNT=$?
-if [ $ERROR_COUNT -ne 1 ]
-then 
-    echo "[FAIL] Expected 1 issue, but got {$ERROR_COUNT}."
-else
-    echo "[PASS]."
-    exit 0
-fi
+
+function run {
+    node ../../bin/ts-unused-exports tsconfig.json --exitWithCount --ignorePaths=to-ignore $1
+    ERROR_COUNT=$?
+    if [ $ERROR_COUNT -ne 1 ]
+    then
+        echo "[FAIL] Expected 1 issue, but got {$ERROR_COUNT}."
+    else
+        echo "[PASS]."
+    fi
+}
+
+run
+run --showLineNumber
