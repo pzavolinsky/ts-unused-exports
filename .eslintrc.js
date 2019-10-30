66d780078ef6b63d2f516ebb98d6030704792592
sean <str_ie@yahoo.co.uk>
2019-10-30 03:04:59 +0100

    Adjust eslint config and turn on autofix in VC

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,10 @@ module.exports =  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
   },
+  "overrides": [
+    {
+      "files": ["src/**/*.ts"],
+      "excludedFiles": "",
+    }
+  ]
 };
