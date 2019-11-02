7c589145568eeb6d320a0de25e36cf72639670fd
sean <str_ie@yahoo.co.uk>
2019-11-02 18:03:14 +0100

    Improve itests so they check for 2 issues, so not just passing by coincidence with the new eslint exit code

--- ispec/_run-and-check-exit-code.sh
+++ ispec/_run-and-check-exit-code.sh
@@ -4,9 +4,9 @@ set +e;
 function run {
     node ../../bin/ts-unused-exports tsconfig.json --exitWithCount --ignorePaths=to-ignore $1
     ERROR_COUNT=$?
-    if [ $ERROR_COUNT -ne 1 ]
+    if [ $ERROR_COUNT -ne 2 ]
     then
-        echo "[FAIL] Expected 1 issue, but got {$ERROR_COUNT}."
+        echo "[FAIL] Expected 2 issues, but got {$ERROR_COUNT}."
     else
         echo "[PASS]."
     fi
