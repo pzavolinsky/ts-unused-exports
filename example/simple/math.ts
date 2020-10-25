ea90d3be220ae532be911fe70c2b8fc6a9b0fcf3
Sean Ryan <str_ie@yahoo.co.uk>
2020-10-25 15:52:52 +0100

    Rename itest types for clarity

--- example/simple/math.ts
+++ example/simple/math.ts
@@ -1,6 +1,6 @@
-export function add1(x: number) { return x + 1; }
+export function unused1(x: number) { return x + 1; }
 
 // ts-unused-exports:disable-next-line
-export function add2(x: number) { return x + 2; }
+export function unusedButDisabled(x: number) { return x + 2; }
 
 export default (x: number) => x + 1;
