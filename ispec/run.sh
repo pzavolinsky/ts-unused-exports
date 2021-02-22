d31cebde1f5d76d70fbb4a28b5bcd356414641d3
Sean Ryan <sean.ryan@mendix.com>
2021-02-22 18:13:46 +0100

    Fix test script

--- ispec/run.sh
+++ ispec/run.sh
@@ -29,6 +29,7 @@ popd
 
 # TYPESCRIPT_VERSION is set for some travis builds (see .travis.yml)
 if [ "$TYPESCRIPT_VERSION" = "4" ]
+then
     pushd ../example/simple-new-ts-4-options
     run_itest
     popd
