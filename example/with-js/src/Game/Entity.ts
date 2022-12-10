32e6f4627433bd1fade1277d9aad46100e3b955d
Sean Ryan <sean.ryan@mendix.com>
2022-12-10 19:47:47 +0100

    Add files to repro

---
+++ example/with-js/src/Game/Entity.ts
@@ -0,0 +1,5 @@
+import { Component } from '../Component/Component.js'
+
+export class Entity
+{
+}
---
+++ example/with-js/src/index.js
@@ -0,0 +1,5 @@
+import { Entity } from './Game/Entity'
+import { Player } from './Game/Player.js'
+
+console.log(`Entity: ${Entity}`)
+console.log(`Player: ${Player}`)
