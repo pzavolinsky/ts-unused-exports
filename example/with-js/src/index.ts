10125588dd4a8d60ff04b17a13b2bb8df6fa1641
Sean Ryan <sean.ryan@mendix.com>
2022-12-10 20:21:37 +0100

    Update - index.ts not index.js in src

--- example/with-js/src/index.ts
+++ example/with-js/src/index.ts
@@ -1,5 +1,7 @@
 import { Entity } from './Game/Entity'
 import { Player } from './Game/Player.js'
+import { thisOneIsUsed } from "./util"
 
 console.log(`Entity: ${Entity}`)
 console.log(`Player: ${Player}`)
+console.log(`thisOneIsUsed: ${thisOneIsUsed}`)
